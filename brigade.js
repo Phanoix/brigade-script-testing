7d833b612e8715227685da70270c7c7b1fc5d299
Ilia <phanoix@gmail.com>
2018-08-02 19:27:56 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -2,4 +2,12 @@ const { events } = require("brigadier");
 
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.commit)
+  
+  var hello = new Job("hello", "alpine:3.4")
+  hello.tasks = [
+    "echo Hello",
+    "echo World"
+  ]
+  
+  hello.run()
 })
