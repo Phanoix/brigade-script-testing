db228668f7de0971e370e994aed9bd673f0db20d
Ilia <phanoix@gmail.com>
2018-08-03 00:21:19 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -8,6 +8,7 @@ events.on("push", function(e, project) {
   hello.tasks = [
     "echo Hello",
     "echo World",
+    "apk add curl",
     "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Testing 123\",\"attachments\":[{\"title\":\"Hello World from brigade!\",\"image_url\":\"https://brigade.sh/assets/images/brigade.png\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"   //test rocket chat notification   //test rocket chat notification
   ]
   
