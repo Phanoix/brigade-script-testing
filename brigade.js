725524316301cdecfcc48a313afae9c713384462
Ilia <phanoix@gmail.com>
2018-08-03 11:43:02 -0400

    test docker build

--- brigade.js
+++ brigade.js
@@ -3,6 +3,17 @@ const { events, Job } = require("brigadier");
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.revision.commit)
   
+  var build = new Job("build", "docker:dind")
+  build.privileged = true;
+  build.env.COMMIT = e.revision.commit
+  build.tasks = [
+    "dockerd-entrypoint.sh &", // Start the docker daemon
+    "sleep 20", // Grant it enough time to be up and running
+    "git checkout https://github.com/gctools-outilsgc/gcconnex.git",
+    "cd gcconnex/",
+    "Docker build -t gcconnex:$COMMIT ."
+  ]
+  
   var hello = new Job("hello", "alpine:3.4")
   hello.env.CHATKEY = project.secrets.chatKey
   hello.tasks = [
@@ -10,8 +21,8 @@ events.on("push", function(e, project) {
     "echo World",
     "apk update",
     "apk add curl",
-    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Testing 123\",\"attachments\":[{\"title\":\"Hello World from brigade!\",\"image_url\":\"https://brigade.sh/assets/images/brigade.png\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"   //test rocket chat notification   //test rocket chat notification
+    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Testing Brigade\",\"attachments\":[{\"title\":\"Brigade build finished!\",\"image_url\":\"https://brigade.sh/assets/images/brigade.png\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"      //test rocket chat notification
   ]
   
-  hello.run()
+  build.run().then(() => { hello.run() })
 })
