877b043870e840397c3faee9a3a99926c933616e
Ilia <phanoix@gmail.com>
2018-08-03 13:24:01 -0400

    try pushing built image

--- brigade.js
+++ brigade.js
@@ -6,6 +6,8 @@ events.on("push", function(e, project) {
   var build = new Job("build", "docker:dind")
   build.privileged = true;
   build.env.COMMIT = e.revision.commit
+  build.env.DOCKER_USER = project.secrets.dockerUsr
+  build.env.DOCKER_PASS = project.secrets.dockerPass
   build.tasks = [
     "dockerd-entrypoint.sh &", // Start the docker daemon
     "sleep 20", // Grant it enough time to be up and running
@@ -13,7 +15,9 @@ events.on("push", function(e, project) {
     "apk add git",
     "git clone https://github.com/gctools-outilsgc/gcconnex.git",
     "cd gcconnex/",
-    "docker build -t gcconnex:$COMMIT ."
+    "docker build -t phanoix/gcconnex:test-$COMMIT .",
+    "docker login -u $DOCKER_USER -p $DOCKER_PASS",
+    "docker push phanoix/gcconnex:test-$COMMIT"
   ]
   
   var hello = new Job("hello", "alpine:3.4")
@@ -23,7 +27,7 @@ events.on("push", function(e, project) {
     "echo World",
     "apk update",
     "apk add curl",
-    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Testing Brigade\",\"attachments\":[{\"title\":\"Brigade build finished!\",\"image_url\":\"https://brigade.sh/assets/images/brigade.png\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"      //test rocket chat notification
+    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Brigade build finished.\",\"attachments\":[{\"title\":\"Brigade build finished!\",\"title_link\": \"https://hub.docker.com/r/phanoix/gcconnex/tags/\",\"text\": \"New image available at Docker hub.\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"      //test rocket chat notification
   ]
   
   build.run().then(() => { hello.run() })
