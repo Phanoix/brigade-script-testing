b825b2f319cb015e9c7c943ad8ea3276169d2a89
Ilia <phanoix@gmail.com>
2018-08-02 20:12:54 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -3,10 +3,12 @@ const { events, Job } = require("brigadier");
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.commit)
   
+  hello.env.CHATKEY = project.secrets.chatKey
   var hello = new Job("hello", "alpine:3.4")
   hello.tasks = [
     "echo Hello",
-    "echo World"
+    "echo World",
+    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Testing 123\",\"attachments\":[{\"title\":\"Hello World from brigade!\",\"image_url\":\"https://brigade.sh/assets/images/brigade.png\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"   //test rocket chat notification   //test rocket chat notification
   ]
   
   hello.run()
