66328f90e07c5c7c42393a24aaf91dc18d164c8d
Phanoix <phanoix@gmail.com>
2020-07-08 17:31:16 -0400

    testing a CD python script

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ import requests
 from kubernetes import client, config
 
 def get_latest_sha(repo):
-    latest = requests.get("https://hub.docker.com/v2/" + repo + "/profile-apollo/tags/latest")
+    latest = requests.get("https://hub.docker.com/v2/repositories/" + repo + "/tags/latest")
     test = json.loads(latest.text)
     print(test["images"][0]["digest"])
 
 
 if __name__ == "__main__":
-    repo = "repositories/digitalcollab"
+    repo = "digitalcollab/profile-apollo"
     get_latest_sha(repo)
 
     config.load_kube_config()
