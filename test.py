680e075c5172de10baa88ffc08591be72cff3573
Phanoix <phanoix@gmail.com>
2020-07-10 11:37:29 -0400

    testing some k8s stuff

--- test.py
+++ test.py
@@ -5,13 +5,32 @@ from kubernetes import client, config
 
 def get_latest_sha(repo):
     latest = requests.get("https://hub.docker.com/v2/repositories/" + repo + "/tags/latest")
-    test = json.loads(latest.text)
-    print(test["images"][0]["digest"])
+    data = json.loads(latest.text)
+    sha = data["images"][0]["digest"]
+    return sha[7:]
 
+def k8s_get_deploy():
+    config.load_incluster_config()
+
+    v1 = client.AppsV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_namespaced_deployment(watch=False, namespace="directory")
+    for i in ret.items:
+        print("=== %s\t%s\t%s\t%s" %
+            (i.spec.replicas, i.metadata.namespace, i.metadata.name, i.spec.template.spec.containers[0].image))
+
+    return ret.items[0]
+
+def patch_deploy():
+    v1 = client.AppsV1Api()
 
 if __name__ == "__main__":
-    repo = "digitalcollab/profile-apollo"
-    get_latest_sha(repo)
+    repo = "digitalcollab/directory"
+    print(get_latest_sha(repo))
 
-    config.load_kube_config()
+    config.load_incluster_config()
+    deploy = k8s_get_deploy()
 
+    print("\n")
+    print("%s\t%s\t%s\t%s" %
+        (deploy.spec.replicas, deploy.metadata.namespace, deploy.metadata.name, deploy.spec.template.spec.containers[0].image))
