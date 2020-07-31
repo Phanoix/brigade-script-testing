9da45a4685c90c253d0fdb7447cec9cf5c0fdc08
Ilia <phanoix@gmail.com>
2020-07-31 10:07:42 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -14,13 +14,5 @@ events.on("image_push", function(e, project) {
     "pip install kubernetes",
     "python test.py"
   ]
-  
-  var notify = new Job("notify", "alpine:3.4")
-  notify.env.CHATKEY = project.secrets.chatKey
-  notify.tasks = [
-    "apk update && apk add curl",
-    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Brigade test update finished.\",\"attachments\":[{\"title\":\"Brigade update finished!\",\"title_link\": \"https://hub.docker.com/r/phanoix/gcconnex/tags/\",\"text\": \"New image available at Docker hub.\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"      //test rocket chat notification
-  ]
-  
-  update.run().then(() => { notify.run() })
+
 })
