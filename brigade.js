9c267fcb3c43e8b93af65d47d70d1c96284215d0
Ilia <phanoix@gmail.com>
2020-09-17 10:49:11 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -4,6 +4,15 @@ const yaml = require("js-yaml");
 
 const checkRunImage = "brigadecore/brigade-github-check-run:latest"
 
+const kubeConfig = new kubernetes.KubeConfig();
+kubeConfig.loadFromDefault();
+
+const k8sCoreClient = kubeConfig.makeApiClient(kubernetes.Core_v1Api);
+const k8sAppClient = kubeConfig.makeApiClient(kubernetes.Apps_v1Api);
+
+const BRIGADE_NAMESPACE = "brigade";
+
+
 events.on("check_suite:requested", checkRequested)
 events.on("check_suite:rerequested", checkRequested)
 events.on("check_run:rerequested", checkRequested)
