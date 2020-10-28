897c6d9c8bdecb87ec1568944226557fa28b3033
Ilia <phanoix@gmail.com>
2020-10-28 11:30:15 -0400

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -10,7 +10,7 @@ events.on("pull_request:closed", cleanupResources)
 function createBuildJob(commit, p){
   // build the new container and tag with git commit hash
   var build = new Job("build", "docker:dind")
-  build.privileged = true;
+  build.privileged = true
   build.env.COMMIT = commit
   build.tasks = [
     "dockerd-entrypoint.sh &", // Start the docker daemon
